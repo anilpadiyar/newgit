d85ec416f07b19c32f5752dd1c8c419596e2647d
admin <anilpadiyar12@gmail.com>
2022-07-17 19:25:32 +0530

    this ismy first code pust in git

---
+++ Mongtest.py
@@ -0,0 +1,13 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://ineuron:mongodb123@cluster0.goi2j.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d ={
+    'name':'anil',
+    'email':'apadiyar@csc.com  ',
+    'surname':'padiyar'
+}
+
+db1 =client[]
